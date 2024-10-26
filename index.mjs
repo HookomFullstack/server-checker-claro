89b21e171e159ec635415b2197348c6207c160fc
MiguelFullStack <miguel.fullstack@gmail.com>
2024-10-25 21:29:00 -0400

    test1

---
+++ index.mjs
@@ -0,0 +1,49 @@
+import {createServer} from 'http'
+
+import morgan from 'morgan';
+import express  from 'express'
+import cors  from 'cors'
+import { Server } from 'socket.io'
+import 'dotenv/config'
+
+import { checker } from './scrap.mjs';
+
+// routes
+
+const app = express()
+
+app.use(cors({origin: '*'}))
+app.use(express.json({limit: '100mb'}))
+app.use(morgan('common'))
+
+const PORT = 3000
+
+const httpServer = createServer(app)
+
+export const io = new Server( httpServer, { cors: '*', maxHttpBufferSize: 1e10 } )
+
+app.get('*', async(req ,res) => {
+    res.json({ok: true})
+})
+
+io.on('connection', async(socket) => {    
+    // socket.on('test', () => console.log('first'))
+    try {
+        
+        const instancias = socket.handshake?.query['instancias']
+        
+        if(instancias == 0) return socket.disconnect() 
+            const roomsInstancias = [...Array(Number(instancias))].map((e, i) => e=++i).map(async(id) => await socket.join(id))
+            socket.on('[claro] postpago', async({phones, i, indexingPhones}) => {
+                checker({ phones, io, i, indexingPhones })
+            })
+    } catch (error) {
+        console.log(error);
+    }
+
+})
+
+
+// seed(1000)
+// export const handler = serverless(httpServer)
+httpServer.listen(PORT ?? 0, () => console.log(`conectado al servidor ${PORT}`) )
---
+++ scrap.mjs
@@ -0,0 +1,130 @@
+import puppeteer from 'puppeteer'
+import axios from 'axios'
+import proxyChain from 'proxy-chain'
+export const checker = async({phones, io, i, indexingPhones}) => {
+    const initBrowser = async() => {
+        // const {data} = await axios('http://192.168.1.42:4000/api/batch_get_ip?num=1&country=CO&state=Bogota D.C.&city=random&isp=random&zip=random&t=txt&port=50000&auth=0')
+        // const proxy = await proxyChain.anonymizeProxy({url: `http://${data}`, port: 3000})
+        const browser = await puppeteer.launch({ headless: false,  
+            // args: [ `--proxy-server=${proxy}` ]
+            
+        })
+        const page = await browser.newPage()
+        page.setDefaultNavigationTimeout(0)
+        page.setDefaultTimeout(0)
+        await page.goto('https://portalpagos.claro.com.co/phrame.php?action=despliegue_personal&clase=vistasclaro&metodo=pantalla_inicio&empresa=claro#no-back-button')
+        io.to(i).emit('[claro] exectMsg', `Instancia ${i} ejecutandose....`)
+        await page.waitForNavigation()
+        await page.waitForSelector('h1')
+        const ifAcessDenied = await page.$('h1')
+        const acessDeniedText = await page.evaluate((e) => e?.innerText, ifAcessDenied)
+        console.log(acessDeniedText != 'Portal de PAGOS Y RECARGAS')
+        if(acessDeniedText != 'Portal de PAGOS Y RECARGAS' ) {
+                console.log('entro')
+                await browser.close()
+                return { page: false, browser: false}
+        }
+        return { page, browser }
+    }
+    let page = false
+    let browser = false
+    do {
+        const data = await initBrowser()
+        page = data.page
+        browser = data.browser
+    } while (page == false)
+        
+    for (const phone of phones) {
+        try {
+            await page.waitForSelector('#select > div > h1')
+            const start = new Date();
+            const {price, status} = await page.evaluate(async (phone) => {
+                const myHeaders = new Headers();
+                const formdata = new FormData();
+                formdata.append("FLUJOPAGOGW", "PFPHM");
+                formdata.append("FLUJOPAGO", "10002");
+                formdata.append("ex", "");
+                formdata.append("paramLista", "");
+                formdata.append("action", "despliegue_personal");
+                formdata.append("campos_borrar", "CLACO_NUMERO");
+                formdata.append("operacion", "Adicionar");
+                formdata.append("url_modal", "");
+                formdata.append("id_objeto", "10002");
+                formdata.append("valor_llave", "");
+                formdata.append("parametros_padre", "&ValorTotal=&SaldoParcial=&NumeroCelular=&FechaVencimiento=&FORMA_PAGO=&VALOR_PLAN=&VALOR_CUOTEQUIP=&FECHA_CARGADATOS=&CODIGO_CLIENTE=");
+                formdata.append("mensaje_error", "Debe asignar a un responsable para la actividad a seguir");
+                formdata.append("go", "");
+                formdata.append("preguardar", "");
+                formdata.append("confGuardaComo", "Seguro que desea duplicar este registro");
+                formdata.append("modifi_detalle", "");
+                formdata.append("nombre_campo", "");
+                formdata.append("latitud", "");
+                formdata.append("longitud", "");
+                formdata.append("enviarForm", "");
+                formdata.append("autoguardar", "");
+                formdata.append("guion_encuesta", "");
+                formdata.append("clase", "vistasclaro");
+                formdata.append("metodo", "confirmacion");
+                formdata.append("empresa", "claro");
+                formdata.append("NumeroCelular", `${phone}`);
+                formdata.append("CLACO_NUMERO", "");
+                formdata.append("NRO_CUENTA", "");
+                formdata.append("TIPO_TRANS", "");
+                formdata.append("USRIO_NUMERO", "");
+                formdata.append("IPTRANSACCION", "");
+                formdata.append("TIPO_TRANS_ORIG", "");
+                formdata.append("FECHA_INICIO", "2024-07-13 02:21");
+                formdata.append("NumeroIdentificacion", "");
+                formdata.append("CodigoCliente", "");
+                formdata.append("IdentificacionDeudor", "");
+                formdata.append("OrigenPago", "");
+                formdata.append("mySubmit_", "Continuar");
+        
+                const requestOptions = {
+                    method: "POST",
+                    headers: myHeaders,
+                    body: formdata,
+                    redirect: "follow"
+                };
+        
+                const response = await fetch("https://portalpagos.claro.com.co/phrame.php?id_objeto=100002", requestOptions);
+        
+                const price = await response.text();
+                const parsePrice = price.split('\n')
+                .filter(e => e.includes('ValorTotalEdit'))
+                .map(e => e.split(/[<>]/g))
+                .flat()
+                .filter(e => e.includes('$'))[0]?.split(',')[0].replaceAll('$', '').replaceAll('.', '')
+                console.log(parsePrice)
+                return {price: parsePrice,
+                    status: response.status
+                };
+            }, phone)
+            console.log(price, status)
+            if(status == 403) await page.reload()
+            if(status == 403) {
+                io.to(i).emit('[claro] exectMsg', `HA OCURRIDO UN ERROR CON LA INSTANCIA ${i} SE CERRARA AUTOMATICAMENTE`)
+                throw('error 403 ')
+            }
+            const cookiesInDelete = await page.cookies('https://portalpagos.claro.com.co/index.php?view=vistas/personal/claro/newclaro/inicio.php&id_objeto=#no-back-button')
+            cookiesInDelete.map(async({name}) => await page.deleteCookie({name}))
+            const end = new Date();
+            const time = end.getTime()-start.getTime()+'ms'
+            if (price) {
+                await io.to(i).emit(`[claro] live`, {price, time, index: indexingPhones.indexOf(phone), phone})
+                continue
+            }
+            await io.to(i).emit(`[claro] dead`, {time, index: indexingPhones.indexOf(phone), phone})
+        } catch (error) {
+            await browser.close()
+            do {
+                const data = await initBrowser()
+                page = data.page
+                browser = data.browser
+            } while (page == false);
+            continue
+        }
+    }
+
+
+}
